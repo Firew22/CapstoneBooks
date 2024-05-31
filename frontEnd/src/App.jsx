8841283127af8744725ab61e67cfe6a218fac786
Firew Handiso <firewh18@gmail.com>
2024-05-31 11:42:53 -0600

    creates pages files

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>App</div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
   )
 }
 
