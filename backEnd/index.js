2a5202347e2bcf3e9af4e7ffe48376242b509a1d
Firew Handiso <firewh18@gmail.com>
2024-05-31 10:40:55 -0600

    comets and clean up

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,54 +1,22 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import { PORT, mongoDBURL } from './config.js';
-import Book from './models/bookModels.js';
+import booksRoutes from './routes/booksRoutes.js';
+import cors from 'cors'
 
 const app = express();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+app.use(cors());
+
 app.get('/', (req, res) => {
     res.send('hello world');
 });
 
-// Route to save a new book
-app.post('/books', async (req, res) => {
-    try {
-        const { title, author, publishedYear } = req.body;
-
-        if (!title || !author || !publishedYear) {
-            return res.status(400).send({
-                message: 'Please fill all fields',
-            });
-        }
-
-        const book = new Book({
-            title,
-            author,
-            publishedYear,
-        });
-
-        await book.save();
-
-        return res.status(201).send(book);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).send({ message: error.message });
-    }
-});
-
-// Route to get all books
-
-app.get('/books', async (req, res) => {
-    try {
-        const books = await Book.find();
-        return res.status(200).send(books);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).send({ message: error.message });
-    }
-});
+// Use the books routes
+app.use('/books', booksRoutes);
 
 mongoose
     .connect(mongoDBURL, {
