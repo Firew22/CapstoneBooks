7bd554a732dc918ebc97d80d85d49fd849de2cc2
Firew Handiso <firewh18@gmail.com>
2024-06-03 13:33:46 -0600

    fixed some bugs and created the booksCard and booksTable filesand some new futures

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import CreateBooks from './pages/CreateBooks'
+import DeleteBook from './pages/DeleteBook'
+import EditBook from './pages/EditBook'
+import ShowBooks from './pages/ShowBooks'
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
+      <Route path="/books/create" element={<CreateBooks/>} />
+      <Route path="/books/details/:id" element={<ShowBooks />} />
+      <Route path="/books/delete/:id" element={<DeleteBook />} />
+      <Route path="/books/edit/:id" element={<EditBook />} />
     </Routes>
   )
 }
